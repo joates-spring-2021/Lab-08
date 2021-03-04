8ee6a82f4ebdebee230f4bc664f5d21bd205e9dc
joates <77863872+joates-spring-2021@users.noreply.github.com>
2021-03-04 15:00:18 -0800

    Add template and words file

--- main.py
+++ main.py
@@ -1,8 +1,59 @@
+
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb 10 18:18:17 2021
-
-@author: johnw
+johnwoates
+CPSC 223P-01
+Thu Mar 8, 2021
+joates@fullerton.edu
 """
 
-# all of my code goes here
+import random
+
+# Hangman class. Use this if you want. Or create your own
+class Hangman:
+    
+    def __init__(self, word, triesAllowed):
+        pass
+
+    def Guess(self, letter):
+        """Pass in a letter (length = 1) and check to see if it is in the word.
+            If it is, replace blanks in display word with letter and return True
+            If not, decrement the number of tries left and return False
+        """
+        pass
+
+    def GetNumTriesLeft(self):
+        """Return the number of tries left"""
+        pass
+    
+    def GetDisplayWord(self):
+        """Return the display word (with blanks where letters have not been guessed)"""
+        pass
+    
+    def GetLettersUsed(self):
+        """Return a string with the list of letters that have been used"""
+        pass
+
+    def DidIWin(self):
+        """Return True if all letters have been guessed. False otherwise"""
+        pass
+
+    def DrawGallows(self):
+        """Optional: Return string representing state of gallows"""
+        pass
+    
+if __name__=="__main__":
+    # Read all the words from the hangman_words.txt file
+    wordFile = open("hangman_words.txt", "r")
+    
+    # Seed the random number generator with current system time
+    random.seed()
+    
+    # Convert the string of words in wordFile to a list,
+    # then get a random word using
+    # randomIndex = random.randint(min, max)
+    
+    # Instantiate a game using the Hangman class
+    
+    
+    # Use a while loop to play the game
